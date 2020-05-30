77370a519d8a9528e6fb363d7d25de6233ee6575
hasanguzelmansur <hasan.guzelmansur@gmail.com>
2020-05-30 17:44:36 +0300

    Katagoriler anasayfaya eklendi.

---
+++ app/src/main/java/com/example/buygo/CarFragment.java
@@ -0,0 +1,23 @@
+package com.example.buygo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CarFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_car,container,false);
+
+        return viewGroup;
+    }
+}
